c9323917087b6201f5c81a6ac7faf4ab4e86f793
gtb200 <58148686+gtb200@users.noreply.github.com>
2020-08-21 00:58:12 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -24,14 +24,11 @@ function calcularPreco(ev){
         return;
     }
     dias=calcularDias(diasAux);
-    console.log(dias);
-    console.log(preçoFinal);
 
     if(inputs[1].value!=""){
         preçoFinal+= dias[0]*3*inputs[1].value;
         preçoFinal+= dias[1]*15*inputs[1].value;
         preçoFinal+= dias[2]*45*inputs[1].value;
-        console.log(preçoFinal);
     }
     if(inputs[2].value!=""){
         preçoFinal+= dias[0]*4*inputs[2].value;
@@ -47,7 +44,6 @@ function calcularPreco(ev){
 
         preçoFinal+=diasAux*inputs[4].value;
     }
-    console.log(preçoFinal);
     document.getElementsByClassName("resultado")[0].textContent="Preço final: "+preçoFinal+"€";
 }
 function loadImages(){
@@ -83,48 +79,49 @@ function setToggled(ev) {
     imagem.style.marginLeft="10%";
     imagem.style.marginRight="10%";
 
-    console.log(imagem.src);
     if (ev.currentTarget.innerText == "Eletricidade"){
         imagem.src+="9.jpg";
         titulo.textContent="Eletricidade"
-        texto.textContent="Espalhado ao longo do acampamento tem disponível pomadas eletricas" +
-            " quais podera usar livremente. Basta requesitar na receção o adaptador para começar" +
-            " a usar."
+        texto.textContent="Espalhadas ao longo do acampamento, disponibilizamos várias tomadas elétricas que " +
+            "poderá utilizar livremente. Para tal, basta requisitar um adaptador na receção." ;
+
 
     }else if (ev.currentTarget.innerText == "Água quente"){
         imagem.src+="8.jpg";
         titulo.textContent="Água quente"
 
-        texto.textContent="Nos nossos estabelecimentos sanitários, mantidos regularmente" +
-            " em boas condições, temos incluído água quente para qualquer pessoa que pretenda" +
-            " utilizar."
+        texto.textContent="Nos nossos estabelecimentos sanitários, que são mantidos limpos e " +
+            "em boas condições, temos incluída água quente para os nossos clientes poderem utilizar." ;
+
     }else if (ev.currentTarget.innerText == "Internet grátis"){
         imagem.src+="7.jpg";
         titulo.textContent="Internet grátis"
 
-        texto.textContent="Na receção temos Internet gratuita para qualquer pessõa que prete" +
-            "nda utilizar. Tendo lá exposta a sua respetiva palavra-passe."
+        texto.textContent="Os nossos clientes podem usufruir de internet gratuita no hotspot" +
+            " que temos localizado na receção." ;
+
     }else if (ev.currentTarget.innerText == "Excelente localização"){
         imagem.src+="6.jpg";
         titulo.textContent="Excelente localização"
 
-        texto.textContent="O nosso parque sítua se rodeado de ótimas vistas e mar. Tudo isto ajuda" +
-            " a ter uma boa experiência de acampamento, " +
-            "visto que, ter uma zona balnear de excelentes condições acrescenta outra camada ao" +
-            " seu acampamento."
+        texto.textContent="Com vista para o mar e para as paisagens verdes da ilha, acompanhado de uma " +
+            "excelente zona balnear vigiada, o nosso parque oferece uma experiência única de campismo e lazer. " ;
+
     }else if (ev.currentTarget.innerText == "Grelhadores"){
         imagem.src+="5.jpg";
         titulo.textContent="Grelhadores"
 
-        texto.textContent="Ao longo do nosso parque vai encontrar grelhadores qual podera" +
-            " utilizar para efetuar os seus delíciosos churrascos. Estes encontram-se" +
-            " em boas condições e com grelha dísponivel."
+        texto.textContent="Ao longo do nosso parque poderá encontrar zonas de lazer com grelhadores em ótimas" +
+            " condições e com a respetiva grelha disponível, que poderá utilizar para um churrasco em família" +
+            " ou um convívio entre amigos. " ;
+
     }else if (ev.currentTarget.innerText == "Parque infantil"){
         imagem.src+="4.jpg";
         titulo.textContent="Parque infantil"
 
-        texto.textContent="Ao lado da receção encontra-se um mini parque infantil em que" +
-            " poderá deixar as suas crianças a divertirem-se."
+        texto.textContent="Perto da receção encontra um miniparque infantil onde as suas crianças," +
+            " com certeza, se divertirão. " ;
+
     }
 
 
